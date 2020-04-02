539584c245ae284f18569d999228d4d6290b1e91
Alanhou1222 <alanhou911222@gmail.com>
2020-04-02 11:03:53 +0800

    Initial Commit

---
+++ Brainstorming VisualizerTests/Brainstorming_VisualizerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Brainstorming_VisualizerTests.swift
+//  Brainstorming VisualizerTests
+//
+//  Created by Alan Hou on 2020/4/2.
+//  Copyright © 2020 WhateverStudio. All rights reserved.
+//
+
+import XCTest
+@testable import Brainstorming_Visualizer
+
+class Brainstorming_VisualizerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
